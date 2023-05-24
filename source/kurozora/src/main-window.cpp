06d6691d2760d8f85a45aca277ce75dfda182069
= <pietrodd2002@gmail.com>
2023-05-24 19:15:09 +0200

    Moved ui/main-window to widget directory

--- source/kurozora/src/main-window.cpp
+++ source/kurozora/src/main-window.cpp
@@ -5,7 +5,7 @@ namespace kurozora
     MainWindow::MainWindow()
     {
         this->set_title("Kurozora");
-        this->builder = Gtk::Builder::create_from_file("../ui/main-window.ui");
+        this->builder = Gtk::Builder::create_from_file("../ui/widgets/main-window.ui");
         this->window = std::shared_ptr<Gtk::Box>(builder->get_widget<Gtk::Box>("main-window"));
         this->set_child(*this->window);
         this->show();
