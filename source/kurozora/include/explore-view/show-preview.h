2bc4b235abd15c84022b6cd0dfa64a76e9d74b84
Lynxes <pietrodd2002@gmail.com>
2023-06-26 20:16:16 +0200

    ShowPreview: switched to unique_ptr where possible

--- source/kurozora/include/explore-view/show-preview.h
+++ source/kurozora/include/explore-view/show-preview.h
@@ -15,12 +15,12 @@ namespace kurozora
     private:
         int anime_id;
         std::shared_ptr<Gtk::Builder> builder;
-        std::shared_ptr<Gtk::Box> box;
-        std::shared_ptr<Gtk::Box> loading_overlay;
-        std::shared_ptr<Gtk::Label> anime_title;
-        std::shared_ptr<Gtk::Label> anime_subtitle;
-        std::shared_ptr<Gtk::Button> control_button;
-        std::shared_ptr<Gtk::Picture> preview_picture;
+        std::unique_ptr<Gtk::Box> box;
+        std::unique_ptr<Gtk::Box> loading_overlay;
+        std::unique_ptr<Gtk::Label> anime_title;
+        std::unique_ptr<Gtk::Label> anime_subtitle;
+        std::unique_ptr<Gtk::Button> control_button;
+        std::unique_ptr<Gtk::Picture> preview_picture;
         std::shared_ptr<Glib::Bytes> downloaded_buffer;
         std::optional<std::shared_ptr<Gdk::Texture>> downloaded_texture;
         std::shared_ptr<Glib::Dispatcher> download_completed;
