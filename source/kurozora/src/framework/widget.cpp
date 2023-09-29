995f850853a6b7aa21bfca5f3e59381c4be217fa
Lynxes <pietrodd2002@gmail.com>
2023-09-30 00:05:23 +0200

    WidgetLoader Helper can now handle static CSS files

--- source/kurozora/src/framework/widget.cpp
+++ source/kurozora/src/framework/widget.cpp
@@ -3,7 +3,13 @@
 namespace kz::fw
 {
 
-    void WidgetLoader::addCssFile(std::string gresource_path) {}
+    void WidgetLoader::addCssFile(std::string gresource_path)
+    {
+        auto css_provider = Gtk::CssProvider::create();
+        css_provider->load_from_resource(gresource_path);
+        auto display = this->get_style_context();
+        display->add_provider_for_display(display->get_display(), css_provider, GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+    }
 
     void WidgetLoader::addCssClass(std::string css_code, std::string class_name)
     {
